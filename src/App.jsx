8671fc44890fb3bbea8e6215209d83f44d63486d
Kayk <urielkaykcontato@gmail.com>
2025-07-05 13:29:00 -0300

    fds

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from './titulo.jsx'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Titulo />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Titulo.jsx
@@ -0,0 +1,8 @@
+
+const titulo = () => {
+  return (
+    <div className ='text-3xl text-center font-bold text-6xl'><h1>vai tomar no cu</h1></div>
+  )
+}
+
+export default titulo
